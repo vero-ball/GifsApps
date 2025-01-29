ccd1e02fce03b85f54bcc38e0d303266ca073741
vero-ball <v.rivadulla.deschamps@gmail.com>
2025-01-29 11:13:10 +0100

    search-list

---
+++ src/app/gifs/components/list/list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gif-list',
+  standalone: false,
+
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class GifListComponent {
+
+}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box-component';
+import { GifListComponent } from './components/list/list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    GifListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomePageComponent,
+    SearchBoxComponent,
+    GifListComponent
   ]
 })
 export class GifsModule { }
